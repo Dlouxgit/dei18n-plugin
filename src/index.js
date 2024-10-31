160d794de994dafd527855ec66795322d4647668
Dlouxgit <470490778@qq.com>
2024-10-31 19:01:07 +0800

    pref: remove much dom

--- src/index.js
+++ src/index.js
@@ -2,27 +2,30 @@ let createI18n
 
 const isI18nDebug = JSON.parse(localStorage.getItem('isI18nDebug') || 'false')
 
+let I18N_MAP
+
 // 监听右键操作
 document.addEventListener('contextmenu', (event) => {
-  if (!isI18nDebug)
-    return
-  const targetText = event.target?.textContent
+  if (isI18nDebug) {
+    event.preventDefault() // 阻止默认的右键行为
+    const targetText = event.target?.textContent
 
-  // 使用正则表达式匹配所有以 ** 开头并以 && 结尾的文本
-  const regex = /(\*\*.*?&&)/g // 匹配 **key&& 的模式
-  const matches = []
-  let match
+    // 使用正则表达式匹配所有以 ** 开头并以 && 结尾的文本
+    const regex = /(\*\*.*?&&)/g // 匹配 **key&& 的模式
+    const matches = []
+    let match
 
-  // 提取所有匹配的文本
-  while ((match = regex.exec(targetText)) !== null) {
-    matches.push(match[1].trim()) // 将匹配的键添加到数组中
-  }
+    // 提取所有匹配的文本
+    while ((match = regex.exec(targetText)) !== null) {
+      matches.push(match[1].trim()) // 将匹配的键添加到数组中
+    }
 
-  const keys = matches.map((item) => {
-    return window.I18N_MAP[item]?.key
-  })
-  if (keys.length > 0) {
-    createIframe(keys) // 创建 iframe
+    const keys = matches.map((item) => {
+      return I18N_MAP[item]?.key
+    })
+    if (keys.length > 0 && keys.every(Boolean)) {
+      createIframe(keys) // 创建 iframe
+    }
   }
 })
 
@@ -83,39 +86,58 @@ let i18n
 
 let messages
 
+let t
+
+let i18nCN
+let i18nTW
+let i18nEN
+let i18nJP
+
+let debugDiv
+
 export function useI18n() {
   if (!i18n || !messages || !createI18n) {
     return 'i18n 初始化失败'
   }
-  const t = i18n.global.t
-  const i18nCN = createI18n({
-    legacy: false,
-    locale: 'zh-CN',
-    fallbackLocale: 'zh-CN',
-    messages,
-    globalInjection: true,
-  })
-  const i18nTW = createI18n({
-    legacy: false,
-    locale: 'zh-TW',
-    fallbackLocale: 'zh-TW',
-    messages,
-    globalInjection: true,
-  })
-  const i18nEN = createI18n({
-    legacy: false,
-    locale: 'en-US',
-    fallbackLocale: 'en-US',
-    messages,
-    globalInjection: true,
-  })
-  const i18nJP = createI18n({
-    legacy: false,
-    locale: 'ja-JP',
-    fallbackLocale: 'ja-JP',
-    messages,
-    globalInjection: true,
-  })
+  if (!t) {
+    t = i18n.global.t
+  }
+  if (!i18nCN) {
+    i18nCN = createI18n({
+      legacy: false,
+      locale: 'zh-CN',
+      fallbackLocale: 'zh-CN',
+      messages,
+      globalInjection: true,
+    })
+  }
+  if (!i18nTW) {
+    i18nTW = createI18n({
+      legacy: false,
+      locale: 'zh-TW',
+      fallbackLocale: 'zh-TW',
+      messages,
+      globalInjection: true,
+    })
+  }
+  if (!i18nEN) {
+    i18nEN = createI18n({
+      legacy: false,
+      locale: 'en-US',
+      fallbackLocale: 'en-US',
+      messages,
+      globalInjection: true,
+    })
+  }
+  if (!i18nJP) {
+    i18nJP = createI18n({
+      legacy: false,
+      locale: 'ja-JP',
+      fallbackLocale: 'ja-JP',
+      messages,
+      globalInjection: true,
+    })
+  }
   const tCN = i18nCN.global.t
   const tTW = i18nTW.global.t
   const tEN = i18nEN.global.t
@@ -127,11 +149,8 @@ export function useI18n() {
       originalResult = `**${t(key, ...args)}&&`
     }
 
-    const div = document.createElement('div')
-    div.textContent = `I18N-${key}-
-    ${originalResult}1`
-    window.I18N_MAP = {
-      ...(window.I18N_MAP || {}),
+    I18N_MAP = {
+      ...(I18N_MAP || {}),
       [originalResult]: {
         zh_CN: tCN(key, ...args),
         zh_TW: tTW(key, ...args),
@@ -144,32 +163,32 @@ export function useI18n() {
     return originalResult
   }
 
-  const debugDiv = document.createElement('div')
-  debugDiv.style.position = 'fixed'
-  debugDiv.style.top = '50px'
-  debugDiv.style.left = '50%'
-  debugDiv.style.transform = 'translateX(-50%)'
-  debugDiv.style.zIndex = '1000'
-  debugDiv.style.background = 'beige'
-  debugDiv.style.border = '3px dashed'
-  debugDiv.style.padding = '4px'
-  debugDiv.innerHTML = '翻译调试模式'
-  const debugSelect = document.createElement('select')
-  debugSelect.innerHTML = '<option value="true">开启</option><option value="false">关闭</option>'
-  debugSelect.value = isI18nDebug.toString()
-  debugSelect.addEventListener('change', () => {
-    localStorage.setItem('isI18nDebug', debugSelect.value)
-    debugDiv.style.display = debugSelect.value === 'true' ? 'block' : 'none'
-    location.reload() // 重新加载页面
-  })
-  debugDiv.appendChild(debugSelect)
-  const closeButton = document.createElement('button')
-  closeButton.textContent = '关闭'
-  closeButton.addEventListener('click', () => {
-    debugDiv.style.display = 'none'
-  })
-  debugDiv.appendChild(closeButton)
-  document.body.appendChild(debugDiv)
+  if (!debugDiv) {
+    debugDiv = document.createElement('div')
+    debugDiv.style.position = 'fixed'
+    debugDiv.style.top = '50px'
+    debugDiv.style.left = '50%'
+    debugDiv.style.transform = 'translateX(-50%)'
+    debugDiv.style.zIndex = '1000'
+    debugDiv.style.background = 'beige'
+    debugDiv.style.border = '3px dashed'
+    debugDiv.style.padding = '4px'
+    debugDiv.innerHTML = '翻译调试模式（使用 Ctrl + K 控制浮窗显隐）'
+    const debugSelect = document.createElement('select')
+    debugSelect.innerHTML = '<option value="true">开启</option><option value="false">关闭</option>'
+    debugSelect.value = isI18nDebug.toString()
+    debugSelect.addEventListener('change', () => {
+      localStorage.setItem('isI18nDebug', debugSelect.value)
+      location.reload() // 重新加载页面
+    })
+    debugDiv.appendChild(debugSelect)
+    document.body.appendChild(debugDiv)
+    document.addEventListener('keydown', (e) => {
+      if (e.ctrlKey && e.key === 'k') {
+        debugDiv.style.display = debugDiv.style.display === 'block' ? 'none' : 'block'
+      }
+    })
+  }
 
   return { t: customT, isI18nDebug, debugDiv }
 }
