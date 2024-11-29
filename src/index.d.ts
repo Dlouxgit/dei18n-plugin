a25ae304fb337b3dea86de1faebeee81cd5cc4a6
Dlouxgit <470490778@qq.com>
2024-11-29 18:17:33 +0800

    chore: fix type

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,4 +2,4 @@ import type { createI18n, useI18n as vueI18nUseI18n } from 'vue-i18n'
 
 export declare const useI18n: typeof vueI18nUseI18n
 
-export declare const getI18nPluginInstall: (ins: ReturnType<createI18n>, data: Record<string, any>, string) => ReturnType<typeof createI18n>
+export declare const getI18nPluginInstall: (ins: ReturnType<createI18n>, data: Record<string, any>, url: string, fn: typeof createI18n) => ReturnType<typeof createI18n>
