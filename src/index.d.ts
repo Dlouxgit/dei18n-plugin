3b3016ebc10610e9faed71a22c3cae1f3851e903
Dlouxgit <470490778@qq.com>
2024-11-29 18:12:42 +0800

    chore: add type

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,3 +1,5 @@
-import type { useI18n as vueI18nUseI18n } from 'vue-i18n'
+import type { createI18n, useI18n as vueI18nUseI18n } from 'vue-i18n'
 
 export declare const useI18n: typeof vueI18nUseI18n
+
+export declare const getI18nPluginInstall: (ins: ReturnType<createI18n>, data: Record<string, any>, string) => ReturnType<typeof createI18n>
